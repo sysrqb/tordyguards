5b7a277a0ef3b2ab0531b704a6f6fd8a37a99709
Matthew Finkel <Matthew.Finkel@gmail.com>
2015-02-12 07:16:21 +0000

    Add abstract connection manager class

---
+++ src/conn_manager.py
@@ -0,0 +1,16 @@
+
+class ConnectionManager(object):
+    def parse_args(self):
+        pass
+    def get_bssid(self):
+        pass
+    def get_essid(self):
+        pass
+    def get_ebssid(self):
+        pass
+    def need_devices(self):
+        pass
+    def set_devices(self, devs):
+        pass
+    def should_continue(self, *kargs):
+        pass
