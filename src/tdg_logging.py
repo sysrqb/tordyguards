8699def7d80b3e1f7b72daf2b7c695b78db28ddc
Matthew Finkel <Matthew.Finkel@gmail.com>
2015-02-20 02:26:24 +0000

    Rename variable: In logger module, h -> handler

--- src/tdg_logging.py
+++ src/tdg_logging.py
@@ -2,11 +2,11 @@ import logging
 from logging import handlers
 
 logger = logging.getLogger(__name__)
-h = handlers.SysLogHandler(address='/dev/log')
+handler = handlers.SysLogHandler(address='/dev/log')
 logger.setLevel(logging.DEBUG)
 formatter = logging.Formatter(
     'tordyguards.%(module)s: %(levelname)s %(message)s')
-h.setFormatter(formatter)
-logger.addHandler(h)
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 
